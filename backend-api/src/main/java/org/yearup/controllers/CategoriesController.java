c11bc4290da0f94d97ffb609b5ef358618382bdd
Coral Estrada <coralestrada28@gmail.com>
2025-12-16 08:42:50 -0600

    Finished completing annotation tasks. Still need to test run.

--- backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
+++ backend-api/src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,8 +1,8 @@
 package org.yearup.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.yearup.data.CategoryDao;
 import org.yearup.data.ProductDao;
 import org.yearup.models.Category;
@@ -11,29 +11,40 @@ import org.yearup.models.Product;
 import java.util.List;
 
 // add the annotations to make this a REST controller
-// add the annotation to make this controller the endpoint for the following url
-    // http://localhost:8080/categories
+@RestController
+
 // add annotation to allow cross site origin requests
+@CrossOrigin("origins = http://localhost:8080")
+
 public class CategoriesController
 {
     private CategoryDao categoryDao;
     private ProductDao productDao;
 
-
     // create an Autowired controller to inject the categoryDao and ProductDao
+    // CHECK HERE
+    @Autowired
+    public CategoriesController(CategoryDao categoryDao, ProductDao productDao){
+        this.categoryDao = categoryDao;
+        this.productDao = productDao;
+    }
+
 
     // add the appropriate annotation for a get action
+
+    @GetMapping("/categories")
     public List<Category> getAll()
     {
         // find and return all categories
-        return null;
+        return categoryDao.getAllCategories();
     }
 
     // add the appropriate annotation for a get action
+    @GetMapping("/categories/{id}")
     public Category getById(@PathVariable int id)
     {
         // get the category by id
-        return null;
+        return categoryDao.getById(id);
     }
 
     // the url to return all products in category 1 would look like this
@@ -42,29 +53,38 @@ public class CategoriesController
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
         // get a list of product by categoryId
-        return null;
+        return productDao.listByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
+    @PostMapping
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('Admin')")
     public Category addCategory(@RequestBody Category category)
     {
-        // insert the category
-        return null;
+        // insert the category- MAKE SURE TO RETURN CATEGORY IN CREATE CATEGORY IN DAOCLASS
+        return categoryDao.create(Category);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
+    @PutMapping("/categories/{id}")
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('Admin')")
     public void updateCategory(@PathVariable int id, @RequestBody Category category)
     {
-        // update the category by id
+        // update the category by id- CHECK AND UPDATE
+        category.setCategoryId(id);
+        categoryDao.update(Category);
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
+    @DeleteMapping("/categories/{id}")
     // add annotation to ensure that only an ADMIN can call this function
+    @PreAuthorize("hasRole('Admin')")
     public void deleteCategory(@PathVariable int id)
     {
-        // delete the category by id
+        // delete the category by id- CHECK AND UPDATE
+        categoryDao.delete(Category);
     }
 }
